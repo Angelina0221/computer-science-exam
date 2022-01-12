af68bfad70cfc6692d06b8a63849afbc07925281
user <user@userde-MacBook-Air.local>
2022-01-12 12:29:31 +0800

    questions 3 - 15

--- exam.py
+++ exam.py
@@ -6,7 +6,6 @@ print(answer)
 
 #Q2
 import random
-from typing import final 
 random_number= random.randint(0 ,-99)
 print(random_number)
 
@@ -36,11 +35,34 @@ print(alphabet_j)
 
 #Q8
 from array import array
-final_exam = ("i",[98,99,100,89])
-print(final_exam)
+final_exam = array("i",[98,99,100,89])
 
 #Q9
-final_exam.__add__(56)
+final_exam = final_exam.append(100)
 
 #Q10
 final_exam_length = len(final_exam)
+print(final_exam_length)
+
+#Q11
+IGCSE_computer_science_student_names = ["Yujing","Barbie","Angelina","Howard","Arsalon"]
+
+#Q12
+IGCSE_computer_science_student_names.remove("Arsalon")
+
+#Q13
+computer_science_sort_list =IGCSE_computer_science_student_names.sort()
+
+print(computer_science_sort_list)
+
+#Q14
+three_exam_score = {
+    "Angelina":[99,98,98],
+    "Yujing":[99,98,98],
+    "Barbie":[99,98,98],
+    "Arsalon":[99,100,99]
+}
+
+#Q15
+Arsalon_three_exam_score = three_exam_score.get("Arsalon")
+print(Arsalon_three_exam_score)
