7a7ac57d85bed16b902f403d3c93bb72ba70990f
user <user@userde-MacBook-Air.local>
2022-01-12 12:10:09 +0800

    exam

--- exam.py
+++ exam.py
@@ -1,4 +1,46 @@
+#Q1
 def add (number , another_number):
     return number + another_number
 answer = add(24,5)
 print(answer)
+
+#Q2
+import random
+from typing import final 
+random_number= random.randint(0 ,-99)
+print(random_number)
+
+#Q3
+number = 0
+while(number < 4):
+    print("Today, I am outstanding in every way")
+    number = number +1
+
+#Q4
+for number in range(200):
+    print("I become what I think about most of the time")
+
+#Q5
+user_name = input("Enter your name:  ")
+print( "You look great today", user_name)
+
+#Q6
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_length = len(alphabet)
+print(alphabet_length)
+
+#Q7
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_j = alphabet.find("j")
+print(alphabet_j)
+
+#Q8
+from array import array
+final_exam = ("i",[98,99,100,89])
+print(final_exam)
+
+#Q9
+final_exam.__add__(56)
+
+#Q10
+final_exam_length = len(final_exam)
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet_length = len(alphabet)
-print(alphabet_length)
 
+three_exam_score = {
+    "Angelina":[99,98,98],
+    "Yujing":[99,98,98],
+    "Barbie":[99,98,98],
+    "Arsalon":[99,100,99]
+}
+
+#Q15
+Arsalon_three_exam_score = three_exam_score.get("Arsalon")
+print(Arsalon_three_exam_score)
